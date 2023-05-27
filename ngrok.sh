718d6e91b62be02d3a967f3e24d62c57b76ae0dd
alikaramaetu <119459922+alikaramaetu@users.noreply.github.com>
2023-05-27 18:56:16 +0100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/alikaramaetu/ngrok/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -13,8 +13,7 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 22 &>/dev/null &
+./ngrok tcp --region us 22 &>/dev/null &
 echo "======================="
 echo Updating Please Wait
 echo "======================="
@@ -28,6 +27,5 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 sudo service ssh start
 echo "===================================="
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo create root password
-passwd
+passwd 123456789
 echo "===================================="
